2f8e4c68ee568be37b683b176e241d937e0c2609
luwei4403 <466813599@qq.com>
2022-01-26 11:31:05 +0800

    新增了学生类

---
+++ src/main/java/com/yida/gitdemo/entity/Student.java
@@ -0,0 +1,49 @@
+package com.yida.gitdemo.entity;
+
+public class Student {
+    private Integer id;
+    private String name;
+    private String sex;
+
+    public Student(Integer id, String name, String sex) {
+        this.id = id;
+        this.name = name;
+        this.sex = sex;
+    }
+
+    public Student() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", sex='" + sex + '\'' +
+                '}';
+    }
+}
